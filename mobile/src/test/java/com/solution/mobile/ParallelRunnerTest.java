bc725605d597918737363519ca71f3761b7791d4
jesussalatiel@hotmail.com <Juegos1590@>
2022-08-10 19:21:08 -0500

    Added Mobile Module

--- mobile/src/test/java/com/solution/mobile/ParallelRunnerTest.java
+++ mobile/src/test/java/com/solution/mobile/ParallelRunnerTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {
-                "src/test/resources/feature/"
+                "src/test/resources/features/"
         },
         plugin = {
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
